fab449cf1f3cf25007c3d7bbc3aba57e51ca486d
JesusCarvajal <ja_carvajalarias@hotmail.com>
2021-09-03 03:46:07 -0500

    Primera integración responsiva

--- main.js
+++ main.js
@@ -4,5 +4,7 @@ btn.addEventListener("click", function(){
     document.getElementById("barra_lateral").classList.toggle('active');
     document.getElementById("masala_chai").classList.toggle('active');
     document.getElementById("frappe_de_chocolate").classList.toggle('active');
+    document.getElementById("frappe").classList.toggle('active');
+    document.getElementById("Mocha").classList.toggle('active');
 });
 
