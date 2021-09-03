6aff114824b057db71cec4fe1e5d5d407783d857
JesusCarvajal <ja_carvajalarias@hotmail.com>
2021-09-02 21:38:37 -0500

    Primera Version Pagina Principal del sitio

---
+++ main.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".barra_btn");
+
+btn.addEventListener("click", function(){
+    document.getElementById("barra_lateral").classList.toggle('active');
+    document.getElementById("masala_chai").classList.toggle('active');
+    document.getElementById("frappe_de_chocolate").classList.toggle('active');
+});
+
